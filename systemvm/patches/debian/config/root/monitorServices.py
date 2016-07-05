38a36b5da0bcda58d670a135040739b52569a827
Romain Dartigues <romain.dartigues@gmail.com>
2016-07-05 08:40:41 +0200

    refactor monitorServices.py, lots of changes
    Could be simplified by using the stdlib modules logging and json.
    
    Messages by category
    --------------------
    
    +------------+--------+----------+------------+
    | type       | number | previous | difference |
    +============+========+==========+============+
    | convention |      5 |      115 |    -110.00 |
    +------------+--------+----------+------------+
    | refactor   |      1 |        4 |      -3.00 |
    +------------+--------+----------+------------+
    | warning    |      6 |       39 |     -33.00 |
    +------------+--------+----------+------------+
    | error      |      0 |        0 |    =       |
    +------------+--------+----------+------------+
    
    Global evaluation
    -----------------
    Your code has been rated at 9.40/10 (previous run: 3.01/10, +6.39)

--- systemvm/patches/debian/config/root/monitorServices.py
+++ systemvm/patches/debian/config/root/monitorServices.py
@@ -20,45 +20,41 @@
 
 
 
-from ConfigParser import SafeConfigParser
-from subprocess import *
-from os import path
-import time
-import os
+import ConfigParser
 import logging
+import os
+import subprocess
+import time
 
-class StatusCodes:
-    SUCCESS      = 0
-    FAILED       = 1
-    INVALID_INP  = 2
-    RUNNING      = 3
-    STOPPED      = 4
-    STARTING     = 5
+SUCCESS = 0
+FAILED = 1
+INVALID_INP = 2
+RUNNING = 3
+STOPPED = 4
+STARTING = 5
 
-class Log:
-    INFO = 'INFO'
-    ALERT = 'ALERT'
-    CRIT  = 'CRIT'
-    NOTIF = 'NOTIF'
+LOG_INFO = 'INFO'
+LOG_ALERT = 'ALERT'
+LOG_CRIT = 'CRIT'
+LOG_NOTIF = 'NOTIF'
 
-class Config:
-    MONIT_AFTER_MINS = 30
-    SLEEP_SEC = 1
-    RETRY_ITERATIONS = 10
-    RETRY_FOR_RESTART = 5
-    MONITOR_LOG = '/var/log/monitor.log'
-    UNMONIT_PS_FILE = '/etc/unmonit_psList.txt'
+MONIT_AFTER_MINS = 30
+SLEEP_SEC = 1
+RETRY_ITERATIONS = 10
+RETRY_FOR_RESTART = 5
+MONITOR_LOG = '/var/log/monitor.log'
+UNMONIT_PS_FILE = '/etc/unmonit_psList.txt'
 
 
-def getConfig( config_file_path = "/etc/monitor.conf" ):
+def get_config(config_file_path="/etc/monitor.conf"):
     """
     Reads the process configuration from the config file.
     Config file contains the processes to be monitored.
 
     """
     process_dict = {}
-    parser = SafeConfigParser()
-    parser.read( config_file_path )
+    parser = ConfigParser.SafeConfigParser()
+    parser.read(config_file_path)
 
 
     for section in parser.sections():
@@ -70,19 +66,24 @@ def getConfig( config_file_path = "/etc/monitor.conf" ):
 
     return  process_dict
 
-def printd (msg):
+def printd(msg):
     """
     prints the debug messages
+
+    .. Note:: Actually does nothing (return 0).
     """
 
     #for debug
     #print msg
+    # XXX: replace printd as the logging module does everything?
+    # ie: logging.setLevel()
     return 0
 
-    f= open(Config.MONITOR_LOG,'r+')
-    f.seek(0, 2)
-    f.write(str(msg)+"\n")
-    f.close()
+    # XXX: Unreachable code
+    fileobj = open(MONITOR_LOG, 'r+')
+    fileobj.seek(0, 2)
+    fileobj.write(str(msg)+"\n")
+    fileobj.close()
 
 def raisealert(severity, msg, process_name=None):
     """ Writes the alert message"""
@@ -93,88 +94,102 @@ def raisealert(severity, msg, process_name=None):
     else:
         log = '['+severity+']' + " " + msg +"\n"
 
-    logging.basicConfig(level=logging.INFO,filename='/var/log/routerServiceMonitor.log',format='%(asctime)s %(message)s')
+    # XXX: basicConfig should be called once
+    logging.basicConfig(
+        level=logging.INFO,
+        filename='/var/log/routerServiceMonitor.log',
+        format='%(asctime)s %(message)s'
+    )
     logging.info(log)
-    msg = 'logger -t monit '+ log
-    pout = Popen(msg, shell=True, stdout=PIPE)
+    # XXX: why not use an additionnal handler
+    # ie: https://docs.python.org/2/library/logging.handlers.html#sysloghandler
+    subprocess.Popen(
+        ('logger', '-t', 'monit', log),
+        shell=True,
+        stdout=subprocess.PIPE,
+    )
 
 
-def isPidMatchPidFile(pidfile, pids):
+def pid_match_pidfile(pidfile, pids):
     """ Compares the running process pid with the pid in pid file.
         If a process with multiple pids then it matches with pid file
     """
 
-    if pids is None or isinstance(pids,list) != True or len(pids) == 0:
-        printd ("Invalid Arguments")
-        return StatusCodes.FAILED
-    if not path.isfile(pidfile):
+    if not pids or not isinstance(pids, list) or not len(pids):
+        printd("Invalid Arguments")
+        return FAILED
+    if not os.path.isfile(pidfile):
         #It seems there is no pid file for this service
-        printd("The pid file "+pidfile+" is not there for this process")
-        return StatusCodes.FAILED
+        printd("The pid file %s is not there for this process" % pidfile)
+        return FAILED
 
-    fd=None
     try:
-        fd = open(pidfile,'r')
-    except:
-        printd("pid file: "+ pidfile +" open failed")
-        return StatusCodes.FAILED
-
-
-    inp = fd.read()
+        inp = open(pidfile, 'r').read()
+    except IOError:
+        printd("pid file: %s open failed" % pidfile)
+        return FAILED
 
     if not inp:
-        fd.close()
-        return StatusCodes.FAILED
+        return FAILED
 
-    printd("file content "+str(inp))
+    printd("file content %s" % inp)
     printd(pids)
-    tocheck_pid  =  inp.strip()
+    tocheck_pid = inp.strip()
     for item in pids:
-        if str(tocheck_pid) ==  item.strip():
+        if tocheck_pid == item.strip():
             printd("pid file matched")
-            fd.close()
-            return StatusCodes.SUCCESS
+            return SUCCESS
 
-    fd.close()
-    return StatusCodes.FAILED
+    return FAILED
 
-def checkProcessRunningStatus(process_name, pidFile):
-    printd("checking the process " + process_name)
-    cmd = ''
+def process_running_status(process_name, pid_file):
+    '''
+    :param str process_name: name of the process to be checked
+    :param str pid_file: name of the file to check
+    :return: True if PIDs found match pid_file and the PIDs in a list
+    :rtype: tuple
+    '''
+    printd("checking the process %s" % process_name)
     pids = []
-    cmd = 'pidof ' + process_name
-    printd(cmd)
+    cmd = ('pidof', process_name)
+    printd(' '.join(cmd))
 
-    #cmd = 'service ' + process_name + ' status'
-    pout = Popen(cmd, shell=True, stdout=PIPE)
-    exitStatus = pout.wait()
+    pout = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     temp_out = pout.communicate()[0]
 
     #check there is only one pid or not
-    if exitStatus == 0:
+    if pout.returncode == 0:
         pids = temp_out.split(' ')
         printd("pid(s) of process %s are %s " %(process_name, pids))
 
         #there is more than one process so match the pid file
         #if not matched set pidFileMatched=False
         printd("Checking pid file")
-        if isPidMatchPidFile(pidFile, pids) == StatusCodes.SUCCESS:
-            return True,pids
+        if pid_match_pidfile(pid_file, pids) == SUCCESS:
+            return True, pids
 
-    printd("pid of exit status %s" %exitStatus)
+    printd("pid of exit status %s" % pout.returncode)
 
-    return False,pids
+    return False, pids
 
-def restartService(service_name):
+def restart_service(service_name):
+    '''basically a ``service X restart``
 
-    cmd = 'service ' + service_name + ' restart'
-    cout = Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT)
-    return_val = cout.wait()
+    :param str service_name: name of the service to be restarted
+    :return: potential success
+    :rtype: bool
+    '''
+    proc = subprocess.Popen(
+        ('service', service_name, 'restart'),
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
 
-    if return_val == 0:
-        printd("The service " + service_name +" recovered successfully ")
-        msg="The process " +service_name+" is recovered successfully "
-        raisealert(Log.INFO,msg,service_name)
+    if proc.wait() == 0:
+        printd("The service %s recovered successfully" % service_name)
+        msg = "The process %s is recovered successfully" % service_name
+        raisealert(LOG_INFO, msg, service_name)
         return True
     else:
         printd("process restart failed ....")
@@ -183,7 +198,7 @@ def restartService(service_name):
 
 
 
-def checkProcessStatus( process ):
+def process_status(process):
     """
     Check the process running status, if not running tries to restart
     """
@@ -191,197 +206,197 @@ def checkProcessStatus( process ):
     service_name = process.get('servicename')
     pidfile = process.get('pidfile')
     #temp_out = None
-    restartFailed=False
-    pidFileMatched=False
-    pids=''
-    cmd=''
+    restart_failed = False
+    pids = ''
+    cmd = ''
     if process_name is None:
-        printd ("\n Invalid Process Name")
-        return StatusCodes.INVALID_INP
+        printd("\n Invalid Process Name")
+        return INVALID_INP
 
-    status, pids = checkProcessRunningStatus(process_name, pidfile)
+    status, pids = process_running_status(process_name, pidfile)
 
-    if status == True:
+    if status:
         printd("The process is running ....")
-        return  StatusCodes.RUNNING
+        return RUNNING
     else:
         printd("Process %s is not running trying to recover" %process_name)
         #Retry the process state for few seconds
 
-        for i in range(1, Config.RETRY_ITERATIONS):
-            time.sleep(Config.SLEEP_SEC)
+        for i in range(1, RETRY_ITERATIONS):
+            time.sleep(SLEEP_SEC)
 
-            if i < Config.RETRY_FOR_RESTART: # this is just for trying few more times
+            if i < RETRY_FOR_RESTART: # this is just for trying few more times
 
-                status, pids = checkProcessRunningStatus(process_name, pidfile)
-                if status == True:
-                    raisealert(Log.ALERT, "The process detected as running", process_name)
+                status, pids = process_running_status(process_name, pidfile)
+                if status:
+                    raisealert(LOG_ALERT, "The process detected as running", process_name)
                     break
                 else:
                     printd("Process %s is not running checking the status again..." %process_name)
                     continue
             else:
-                msg="The process " +process_name+" is not running trying recover "
-                raisealert(Log.INFO,process_name,msg)
+                msg = "The process " +process_name+" is not running trying recover "
+                raisealert(LOG_INFO, process_name, msg)
 
                 if service_name == 'apache2':
                     # Killing apache2 process with this the main service will not start
                     for pid in pids:
                         cmd = 'kill -9 '+pid
                         printd(cmd)
-                        Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT)
+                        subprocess.Popen(
+                            cmd,
+                            shell=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT,
+                        )
 
-                if restartService(service_name) == True:
+                if restart_service(service_name):
                     break
                 else:
-                    restartFailed = True
+                    restart_failed = True
                     continue
         #for end here
 
-        if restartFailed == True:
-            msg="The process %s recover failed "%process_name
-            raisealert(Log.ALERT,process_name,msg)
+        if restart_failed:
+            msg = "The process %s recover failed "%process_name
+            raisealert(LOG_ALERT, process_name, msg)
 
             printd("Restart failed after number of retries")
-            return StatusCodes.STOPPED
+            return STOPPED
 
-    return  StatusCodes.RUNNING
+    return RUNNING
 
 
-def monitProcess( processes_info ):
+def monitor_process(processes_info):
     """
     Monitors the processes which got from the config file
     """
-    if len( processes_info ) == 0:
+    if not len(processes_info):
         printd("Invalid Input")
-        return  StatusCodes.INVALID_INP
+        return INVALID_INP
 
-    dict_unmonit={}
-    umonit_update={}
-    unMonitPs=False
+    dict_unmonit = {}
+    umonit_update = {}
+    unmonit_ps = False
 
-    if not path.isfile(Config.UNMONIT_PS_FILE):
+    if not os.path.isfile(UNMONIT_PS_FILE):
         printd('Unmonit File not exist')
     else:
         #load the dictionary with unmonit process list
-        dict_unmonit = loadPsFromUnMonitFile()
+        dict_unmonit = load_ps_from_unmonit_file()
 
     #time for noting process down time
-    csec = repr(time.time()).split('.')[0]
+    csec = time.time()
 
-    for process,properties in processes_info.items():
-        #skip the process it its time stamp less than Config.MONIT_AFTER_MINS
-        printd ("checking the service %s \n" %process)
+    for process, properties in processes_info.items():
+        #skip the process it its time stamp less than MONIT_AFTER_MINS
+        printd("checking the service %s \n" %process)
 
-        if not is_emtpy(dict_unmonit):
+        if dict_unmonit:
             if dict_unmonit.has_key(process):
                 ts = dict_unmonit[process]
 
-                if checkPsTimeStampForMonitor (csec, ts, properties) == False:
-                    unMonitPs = True
+                if not check_ps_timestamp_for_monitor(csec, ts, properties):
+                    unmonit_ps = True
                     continue
 
-        if checkProcessStatus( properties) != StatusCodes.RUNNING:
-            printd( "\n Service %s is not Running"%process)
+        if process_status(properties) != RUNNING:
+            printd("\n Service %s is not Running"%process)
             #add this process into unmonit list
-            printd ("updating the service for unmonit %s\n" %process)
-            umonit_update[process]=csec
+            printd("updating the service for unmonit %s\n" %process)
+            umonit_update[process] = csec
 
     #if dict is not empty write to file else delete it
-    if not is_emtpy(umonit_update):
-        writePsListToUnmonitFile(umonit_update)
-    else:
-        if is_emtpy(umonit_update) and unMonitPs == False:
-            #delete file it is there
-            removeFile(Config.UNMONIT_PS_FILE)
+    if umonit_update:
+        write_pslist_to_unmonit_file(umonit_update)
+    elif not umonit_update and not unmonit_ps:
+        #delete file it is there
+        unlink(UNMONIT_PS_FILE)
 
 
-def checkPsTimeStampForMonitor(csec,ts, process):
-    printd("Time difference=%s" %str(int(csec) - int(ts)))
-    tmin = (int(csec) - int(ts) )/60
+def check_ps_timestamp_for_monitor(csec, ts, process):
+    '''
+    :param int csec: actual time in seconds
+    :param int ts: previous time in seconds
+    :param str process: process name
+    :return: True if need to be monitored
+    :rtype: int
+    '''
+    printd("Time difference=%d" % (csec - ts))
+    tmin = (csec - ts)/60
 
-    if ( int(csec) - int(ts) )/60 < Config.MONIT_AFTER_MINS:
-        raisealert(Log.ALERT, "The %s get monitor after %s minutes " %(process, Config.MONIT_AFTER_MINS))
-        printd('process will be monitored after %s min' %(str(int(Config.MONIT_AFTER_MINS) - tmin)))
+    if tmin < MONIT_AFTER_MINS:
+        raisealert(
+            LOG_ALERT,
+            "The %s get monitor after %s minutes " % (
+                process,
+                MONIT_AFTER_MINS,
+            )
+        )
+        printd(
+            'process will be monitored after %s min' % (
+                MONIT_AFTER_MINS - tmin
+            )
+        )
         return False
 
-    return  True
+    return True
 
-def removeFile(fileName):
-    if path.isfile(fileName):
-        printd("Removing the file %s" %fileName)
-        os.remove(fileName)
+def unlink(filename):
+    '''a verbose :func:`os.unlink`
+    '''
+    if os.path.isfile(filename):
+        printd("Removing the file %s" %filename)
+        os.remove(filename)
 
-def loadPsFromUnMonitFile():
-
+def load_ps_from_unmonit_file():
+    '''load unmonit list from UNMONIT_PS_FILE
+    '''
     dict_unmonit = {}
 
     try:
-        fd = open(Config.UNMONIT_PS_FILE)
-    except:
-        printd("Failed to open file %s " %(Config.UNMONIT_PS_FILE))
-        return StatusCodes.FAILED
+        data = open(UNMONIT_PS_FILE).read()
+    except IOError:
+        printd("Failed to open file %s " %(UNMONIT_PS_FILE))
+        return FAILED
 
-    ps = fd.read()
+    if not data:
+        printd("File %s content is empty " %UNMONIT_PS_FILE)
+        return FAILED
 
-    if not ps:
-        printd("File %s content is empty " %Config.UNMONIT_PS_FILE)
-        return StatusCodes.FAILED
-
-    printd(ps)
-    plist = ps.split(',')
-    plist.remove('')
-    for i in plist:
-        dict_unmonit[i.split(':')[0]] = i.split(':')[1]
-
-    fd.close()
+    printd(data)
+    for item in data.split(','):
+        key, val = item.split(':')
+        dict_unmonit[key] = int(val)
 
     return dict_unmonit
 
 
-def writePsListToUnmonitFile(umonit_update):
+# XXX: replace *unmonit_file with json.dump/load?
+def write_pslist_to_unmonit_file(umonit_update):
+    '''write unmonit list to UNMONIT_PS_FILE
+    '''
     printd("Write updated unmonit list to file")
-    line=''
-    for i in umonit_update:
-        line+=str(i)+":"+str(umonit_update[i])+','
+    data = [
+        '%s:%d' % (process_name, seconds)
+        for process_name, seconds in umonit_update.items()
+    ]
+    line = ','.join(data)
     printd(line)
     try:
-        fd=open(Config.UNMONIT_PS_FILE,'w')
-    except:
-        printd("Failed to open file %s " %Config.UNMONIT_PS_FILE)
-        return StatusCodes.FAILED
+        open(UNMONIT_PS_FILE, 'w').write(line)
+    except IOError:
+        printd("Failed to open file %s " %UNMONIT_PS_FILE)
+        return FAILED
 
-    fd.write(line)
-    fd.close()
-
-
-def is_emtpy(struct):
-    """
-    Checks wether the given struct is empty or not
-    """
-    if struct:
-        return False
-    else:
-        return True
 
 def main():
-    '''
-    Step1 : Get Config
-    '''
+    # Step1 : Get Config
     printd("monitoring started")
-    temp_dict  = getConfig()
+    temp_dict = get_config()
 
-    '''
-    Step2: Monitor and Raise Alert
-    '''
-    monitProcess( temp_dict )
+    # Step2: Monitor and Raise Alert
+    monitor_process(temp_dict)
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
