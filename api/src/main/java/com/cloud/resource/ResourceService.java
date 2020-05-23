ff5c5eb459d03bd5a4f0c506e35bb16dd830ccc6
Gabriel Brascher <gabriel@apache.org>
2020-05-23 03:26:10 -0300

    Declare host as dead

--- api/src/main/java/com/cloud/resource/ResourceService.java
+++ api/src/main/java/com/cloud/resource/ResourceService.java
@@ -24,6 +24,7 @@ import org.apache.cloudstack.api.command.admin.cluster.UpdateClusterCmd;
 import org.apache.cloudstack.api.command.admin.host.AddHostCmd;
 import org.apache.cloudstack.api.command.admin.host.AddSecondaryStorageCmd;
 import org.apache.cloudstack.api.command.admin.host.CancelMaintenanceCmd;
+import org.apache.cloudstack.api.command.admin.host.DeclareHostAsDeadCmd;
 import org.apache.cloudstack.api.command.admin.host.PrepareForMaintenanceCmd;
 import org.apache.cloudstack.api.command.admin.host.ReconnectHostCmd;
 import org.apache.cloudstack.api.command.admin.host.UpdateHostCmd;
@@ -66,6 +67,8 @@ public interface ResourceService {
     List<? extends Host> discoverHosts(AddSecondaryStorageCmd cmd) throws IllegalArgumentException, DiscoveryException, InvalidParameterValueException;
 
     Host maintain(PrepareForMaintenanceCmd cmd);
+
+    Host declareHostAsDead(DeclareHostAsDeadCmd cmd);
 
     /**
      * Deletes a host
